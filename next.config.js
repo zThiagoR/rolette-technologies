acab58c5ab5089be98f6182af941b33f4c7fdf54
zThiagoR <thi.magalha13@gmail.com>
2023-12-19 01:24:42 -0300

    chore: add imgur.com to images configuration

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [{
+      hostname: 'i.imgur.com'
+    }],
+  },
+}
 
 module.exports = nextConfig
