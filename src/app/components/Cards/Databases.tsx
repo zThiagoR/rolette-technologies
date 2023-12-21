89d402dacf02d061a2872d90514e0d170844638d
marcelldac <dacmarcell@gmail.com>
2023-12-21 16:42:02 -0300

    feat: apply factory dp in db selector

--- src/app/components/Cards/Databases.tsx
+++ src/app/components/Cards/Databases.tsx
@@ -1,46 +1,46 @@
 import { useEffect, useState } from "react";
-import Image from 'next/image';
-import styles from "../../../styles/page.module.scss";
+import Image from "next/image";
 
-import data from "../../../data/technologies.json";
+import styles from "@/styles/page.module.scss";
+import data from "@/data/technologies.json";
+import { randomDBSelector } from "@/services/randomDBSelector";
+import { dbType } from "@/utils/types";
 
-interface TypeSQL {
-  name: string;
-  image: string;
-}
-
-interface SQLCardProps {
+type dbCardProps = {
   isAnimation: boolean;
   winner: string | null;
-}
+};
 
-export default function DatabasesCard({ isAnimation, winner }: SQLCardProps) {
-  const [selectedSQL, setSelectedSQL] = useState<TypeSQL | null>(null);
+export default function DatabasesCard({ isAnimation, winner }: dbCardProps) {
+  const [selectedDB, setSelectedDB] = useState<dbType | null>(null);
 
   useEffect(() => {
     if (winner) {
-      const databaseOptions = data["Databases"];
-      if (databaseOptions) {
-        const sqlEntries = Object.entries(databaseOptions);
-        const randomSQLEntry = sqlEntries[Math.floor(Math.random() * sqlEntries.length)];
-        setSelectedSQL({ name: randomSQLEntry[0], image: randomSQLEntry[1] as string });
-      } else {
-        setSelectedSQL(null);
-      }
+      const dbOptions = data["Databases"];
+      const selectedDB = dbOptions ? randomDBSelector(dbOptions) : null;
+      setSelectedDB(selectedDB);
     }
   }, [winner]);
 
   return (
     <div className={styles.card_secondary}>
       <h3>Databases</h3>
-      {selectedSQL && winner ? (
-        <div className={`${styles.carousel} ${isAnimation ? styles.animate : ''}`}>
-          <Image src={selectedSQL.image} alt={selectedSQL.name} title={selectedSQL.name} width={64} height={64} />
-          <p>
-            {selectedSQL.name}
-          </p>
+      {selectedDB && winner ? (
+        <div
+          className={`${styles.carousel} ${isAnimation ? styles.animate : ""}`}
+        >
+          <Image
+            src={selectedDB.image}
+            alt={selectedDB.name}
+            title={selectedDB.name}
+            width={64}
+            height={64}
+          />
+          <p>{selectedDB.name}</p>
         </div>
-      ) : <p>Nenhuma tecnologia selecionada</p>}
+      ) : (
+        <p>Nenhuma tecnologia selecionada</p>
+      )}
     </div>
   );
 }
